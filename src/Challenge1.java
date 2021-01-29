d4a67dd5de40656237ae73c33fc344899e9f5ede
Oswaldo Neto <oswaldo.neto@gmail.com>
2021-01-29 13:12:17 -0300

    First version

---
+++ src/Challenge1.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/**
+ * Java Basic Challenge
+ *
+ * Write a code to verify if a string provided has all unique characters.
+ *
+ * For example: "zyyz"
+ *  x -> unique
+ *  y -> not unique
+ *  y -> not unique
+ *  z -> unique
+ * Result: False
+ *
+ */
+public class Challenge1 {
+
+    /**
+     * Your code goes here
+     */
+
+    public static void main(String[] args) {
+        String str = "invillia";
+        System.out.println("Input : "+str);
+        System.out.println("String has all unique characters: " + hasAllUniqueCharacters(str));
+    }
+
+}
+
